50a65a4efe3069422a4447ddfd9c31342c08cff5
Osho Emmanuel <oshoemmanuel3@gmail.com>
2025-11-21 18:29:22 +0100

    Undefined is Undefined

--- 4-concat.js
+++ 4-concat.js
@@ -2,9 +2,5 @@
 const firstArg = process.argv[2];
 const secondArg = process.argv[3];
 
-// Check if both arguments exist
-if (firstArg && secondArg) {
-    console.log(firstArg + " is " + secondArg);
-} else {
-    console.log("Two arguments are required");
-}
+// Print in the requested format
+console.log(firstArg + " is " + secondArg);
