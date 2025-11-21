7ffc4394d7babb3b93515cd37d60ecef25ef7e0d
Osho Emmanuel <oshoemmanuel3@gmail.com>
2025-11-21 18:46:35 +0100

    I love C

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,10 +1,7 @@
-// Get the first argument
 const arg = process.argv[2];
 const times = parseInt(arg, 10);
 
-if (isNaN(times)) {
-    console.log("Missing number of occurrences");
-} else {
+if (!isNaN(times)) {
     let i = 0;
     let output = "";
     while (i < times) {
